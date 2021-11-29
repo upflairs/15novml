51b121b142f17ba80362362cceaa88d2956246a0
upflairspace <87517196+upflairs@users.noreply.github.com>
2021-11-29 14:07:14 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,54 @@
+##a = eval(input('Enter a number:'))
+##if a%7:
+##    print('Not divisable')
+##else:
+##    print('Divisable')
+
+##a = eval(input('first:'))
+##b = eval(input('second:'))
+##c = eval(input('third:'))
+##
+##if (a<=b and a>=c) or (a>=b and a<=c):
+##    print(a,'is second highest')
+##elif (b<=a and b>=c) or (b>=a and b<=c):
+##    print(b,'is second highest')
+##else:
+##    print(c,'is second highest')
+
+##for i in 'nice':
+##    print(i)
+
+##print(5,10,'a',sep='+',end='%')
+##print(5,10,'a')
+
+for i in range(1,6):
+    print('@' * i)
+for i in range(5,0,-1):
+    print('@' * i)
+
+'''
+1
+22
+333
+4444
+55555
+
+1
+12
+123
+1234
+12345
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+    
---
+++ shell.py
@@ -0,0 +1,86 @@
+Python 3.9.4 (tags/v3.9.4:1f2e308, Apr  6 2021, 13:40:21) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:/Users/hp/Desktop/UFT/15novML/day6/script.py ====
+Enter a number:7
+Divisable
+>>> 
+= RESTART: C:/Users/hp/Desktop/UFT/15novML/day6/script.py
+first:34
+second:13
+third:9
+13 is second highest
+>>> 
+= RESTART: C:/Users/hp/Desktop/UFT/15novML/day6/script.py
+first:3
+second:19
+third:9
+9 is second highest
+>>> range(10)
+range(0, 10)
+>>> type(range(10))
+<class 'range'>
+>>> list(range(10))
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+>>> list(range(1,11))
+[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+>>> list(range(100,9,-10))
+[100, 90, 80, 70, 60, 50, 40, 30, 20, 10]
+>>> 
+===== RESTART: C:/Users/hp/Desktop/UFT/15novML/day6/script.py ====
+n
+i
+c
+e
+>>> 
+= RESTART: C:/Users/hp/Desktop/UFT/15novML/day6/script.py
+5 10 a
+>>> 
+= RESTART: C:/Users/hp/Desktop/UFT/15novML/day6/script.py
+5 10 a
+>>> 
+= RESTART: C:/Users/hp/Desktop/UFT/15novML/day6/script.py
+5+10+a
+>>> 
+5= RESTART: C:/Users/hp/Desktop/UFT/15novML/day6/script.py
+5+10+a
+5 10 a
+>>> 
+= RESTART: C:/Users/hp/Desktop/UFT/15novML/day6/script.py
+5+10+a
+5 10 a
+>>> 
+= RESTART: C:/Users/hp/Desktop/UFT/15novML/day6/script.py
+5+10+a%5 10 a
+>>> 
+= RESTART: C:/Users/hp/Desktop/UFT/15novML/day6/script.py
+@
+@@
+@@@
+@@@@
+@@@@@
+>>> 
+= RESTART: C:/Users/hp/Desktop/UFT/15novML/day6/script.py
+@
+@@
+@@@
+@@@@
+@@@@@
+@@@@@
+@@@@
+@@@
+@@
+@
+>>> a = 9
+>>> str(a)
+'9'
+>>> chr(65)
+'A'
+>>> ord(67)
+Traceback (most recent call last):
+  File "<pyshell#9>", line 1, in <module>
+    ord(67)
+TypeError: ord() expected string of length 1, but int found
+>>> ord('B')
+66
+>>> 
